f40155a05649b94053473c45b3b714bb463d3e46
GarfieldSuper163 <141078175+GarfieldSuper163@users.noreply.github.com>
2024-05-28 18:02:13 +0000

    End of Class 5

---
+++ script2.js
@@ -0,0 +1,31 @@
+// declarations
+let commande = document.querySelector(".commande");
+
+// on load
+commande.style.visibility = "true"
+
+// functions
+
+function close_commande(){
+  commande.style.visibility = "false";
+}
+function emporter(){
+
+}
+function Livraison(){
+
+}
+
+function address(){
+
+}
+function emplacement(){
+
+}
+function ville(){
+
+}
+
+function confirmer(){
+  
+}
