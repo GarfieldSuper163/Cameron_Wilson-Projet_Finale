5a7b24cfda9d74ba4bba17f3163f85246dc0d076
GarfieldSuper163 <141078175+GarfieldSuper163@users.noreply.github.com>
2024-06-05 19:27:18 +0000

    End of Class 10

--- script2.js
+++ script2.js
@@ -6,11 +6,38 @@ let emportercommande = document.querySelector(".emporter-commande")
 let livraisoncommande = document.querySelector(".livraison-commande")
 let btnemporter = document.querySelector(".emporter")
 let btnlivraison = document.querySelector(".Livraison")
+let btnrideau = document.querySelector("#rideau")
+let btnbank = document.querySelector("#bank")
+let btnKanata = document.querySelector("#Kanata")
 let btnadd_poutine = document.querySelector(".add_poutine")
 let btnadd_combo = document.querySelector(".add_combo")
+let select = document.getElementById("dateSelector");
+let lstpanier = [];
+let lstserveur = [];
 
+mois = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", 
+  "Aout", "Septembre", "Octobre", "Novembre", "Décembre"];
+
+for(var j = 0; j < 10; j++) {
+  var date = new Date();
+  date.setDate(date.getDate() + j);
+  select.options[select.options.length] = new Option([date.getDate(), mois[date.getMonth()], date.getFullYear()].join(' '), date.toISOString());
+}
 // on load
-// commande.style.visibility = "visible"
+let items = [
+    {
+        quantité: 0,
+        nom: "poutine",
+        image: src = "resources/POUTINE.jpg",
+        prix: 15.49
+    },
+    {
+        quantité: 0,
+        nom: 'combo',
+        image: src = "resources/COMBO.jpg",
+        prix: 20.99
+    }
+]
 
 // functions
 function openCommande() {
@@ -35,6 +62,9 @@ function pourLivraison() {
   typecommande.style.visibility = "hidden";
   emportercommande.style.visibility = "hidden";
   livraisoncommande.style.visibility = "visible";
+  btnrideau.style.border = "0.0625em solid black";
+  btnbank.style.border = "0.0625em solid black";
+  btnKanata.style.border = "0.0625em solid black";
 }
 
 function confirmer() {
@@ -43,6 +73,31 @@ function confirmer() {
   livraisoncommande.style.visibility = "hidden";
 }
 
-function add_to_cart() {
+function rideau() {
+  if (btnrideau.style.border == "0.0625em solid black")
+    btnrideau.style.border = "0.1875em solid red";
+  else if (btnrideau.style.border = "0.1875em solid red")
+    btnrideau.style.border = "0.0625em solid black";
+  btnbank.style.border = "0.0625em solid black";
+  btnKanata.style.border = "0.0625em solid black";
+}
+function bank() {
+  btnrideau.style.border = "0.0625em solid black";
+  if (btnbank.style.border == "0.0625em solid black")
+      btnbank.style.border = "0.1875em solid red";
+  else if (btnbank.style.border = "0.1875em solid red")
+      btnbank.style.border = "0.0625em solid black";
+  btnKanata.style.border = "0.0625em solid black";
+}
+function Kanata() {
+  btnrideau.style.border = "0.0625em solid black";
+  btnbank.style.border = "0.0625em solid black";
+  if (btnKanata.style.border == "0.0625em solid black")
+      btnKanata.style.border = "0.1875em solid red";
+  else if (btnKanata.style.border = "0.1875em solid red")
+      btnKanata.style.border = "0.0625em solid black";
+}
 
+function add_Combo(){
+  
 }
